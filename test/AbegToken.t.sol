9855a06dd1246d205b2477c5bafd74c895149419
Lawrence Ezealor <ezealor20@gmail.com>
2024-04-13 11:38:32 +0100

    functionalities

---
+++ test/AbegToken.t.sol
@@ -0,0 +1,84 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console2, stdJson} from "forge-std/Test.sol";
+
+import {Merkle} from "../src/AbegToken.sol";
+
+contract AbegTest is Test {
+    using stdJson for string;
+    Merkle public merkle;
+    struct Result {
+        bytes32 leaf;
+        bytes32[] proof;
+    }
+
+    struct User {
+        address user;
+        uint tokenId;
+        uint amount;
+    }
+    Result public result;
+    User public user;
+    bytes32 root =
+        0x9e7c204c2a0f3bd18560e700f7b8bad2ebf0a40fa87b240985006315fc2df515;
+
+    address user1 = 0x577Cb3F17e7075D141F3c10bCcA49c9B797AA18A;
+
+    function setUp() public {
+        merkle = new Merkle(root);
+        string memory _root = vm.projectRoot();
+        string memory path = string.concat(_root, "/merkle_tree.json");
+
+        string memory json = vm.readFile(path);
+        string memory data = string.concat(_root, "/address_data.json");
+
+        string memory dataJson = vm.readFile(data);
+
+        bytes memory encodedResult = json.parseRaw(
+            string.concat(".", vm.toString(user1))
+        );
+        user.user = vm.parseJsonAddress(
+            dataJson,
+            string.concat(".", vm.toString(user1), ".address")
+        );
+        user.tokenId = vm.parseJsonUint(
+            dataJson,
+            string.concat(".", vm.toString(user1), ".tokenID")
+        );
+        user.amount = vm.parseJsonUint(
+            dataJson,
+            string.concat(".", vm.toString(user1), ".amount")
+        );
+        result = abi.decode(encodedResult, (Result));
+        console2.logBytes32(result.leaf);
+    }
+
+    function testClaimed() public {
+        bool success = merkle.claim(
+            user.user,
+            user.tokenId,
+            user.amount,
+            result.proof
+        );
+        assertTrue(success);
+    }
+
+    function testAlreadyClaimed() public {
+        merkle.claim(user.user, user.tokenId, user.amount, result.proof);
+        vm.expectRevert("already claimed");
+        merkle.claim(user.user, user.tokenId, user.amount, result.proof);
+    }
+
+    function testIncorrectProof() public {
+        bytes32[] memory fakeProofleaveitleaveit;
+
+        vm.expectRevert("not whitelisted");
+        merkle.claim(
+            user.user,
+            user.tokenId,
+            user.amount,
+            fakeProofleaveitleaveit
+        );
+    }
+}
